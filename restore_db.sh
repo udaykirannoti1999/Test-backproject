a0549c7b4cc91c29aa66b214dac7910f91f5d7cb
udaykirannoti1999 <116997970+udaykirannoti1999@users.noreply.github.com>
2025-09-21 22:38:20 +0530

    Update restore_db.sh

--- restore_db.sh
+++ restore_db.sh
@@ -1,9 +1,10 @@
+
 #!/bin/bash
 set -euo pipefail
 
 # ----------------- CONFIGURATION -----------------
 DB_INSTANCE_IDENTIFIER="$1"
-SECURITY_GROUP_ID="sg-0c7ff10ff513eaaf4"   
+SECURITY_GROUP_IDS=["sg-0c7ff10ff513eaaf4","sg-0ebf411a4d322834f"]
 RESTORE_TIMEOUT_MINUTES=60
 # -------------------------------------------------
 
@@ -54,15 +55,20 @@ check_subnet_group() {
     echo "Subnet group '$DB_SUBNET_GROUP' exists."
 }
 
+# Function: Start restore job
 start_restore_job() {
     echo "Starting restore job..."
-
-    # Metadata must be key=value pairs, comma-separated, no JSON
-    METADATA="DBInstanceIdentifier=${DB_INSTANCE_IDENTIFIER},DBInstanceClass=db.t4g.micro,DBSubnetGroupName=${DB_SUBNET_GROUP},VpcSecurityGroupIds=${SECURITY_GROUP_ID},Port=${DB_PORT},PubliclyAccessible=true"
+    METADATA_JSON=$(jq -n \
+        --arg db "$DB_INSTANCE_IDENTIFIER" \
+        --arg class "db.t4g.micro" \
+        --arg subnet "$DB_SUBNET_GROUP" \
+        --arg sg "$SECURITY_GROUP_IDS" \
+        --arg port "$DB_PORT" \
+        '{DBInstanceIdentifier:$db, DBInstanceClass:$class, DBSubnetGroupName:$subnet, VpcSecurityGroupIds:$sg, Port:$port, PubliclyAccessible:"true"}')
 
     RESTORE_JOB_ID=$(aws backup start-restore-job \
         --recovery-point-arn "$LATEST_RECOVERY_POINT" \
-        --metadata $METADATA \
+        --metadata "$METADATA_JSON" \
         --iam-role-arn "$IAM_ROLE_ARN" \
         --resource-type RDS \
         --region "$AWS_REGION" \
@@ -71,7 +77,6 @@ start_restore_job() {
 
     echo "Restore Job ID: $RESTORE_JOB_ID"
 }
-
 
 # Function: Wait for restore job
 wait_for_restore() {
@@ -179,3 +184,6 @@ update_secret
 update_ecs_service
 wait_for_ecs_service
 # -------------------------------------------------
+Restore failed. Error details:
+Bad metadata argument.
+[Pipeline] }
